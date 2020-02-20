c935c44add418b681adba81be8ff184092b73da2
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-02-20 22:59:41 +0100

    I just created a get request from postman and I made sure the post requests work as well

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.post("/todos", (req, res) => {
   );
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 let PORT = 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
