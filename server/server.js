aa701aa59345671c1d2543ea87ce12db2cbce1e3
BlessedEmoefe <blessedemoefe1122334455@gmail.com>
2020-02-24 14:35:29 +0100

    i just completed a challenge that required me to create a get todo with the id and verifying it

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 let { User } = require("./models/user");
@@ -22,15 +23,34 @@ app.post("/todos", (req, res) => {
   );
 });
 
-app.get("/todos", (req, res) => {
-  Todo.find().then(
-    todos => {
-      res.send({ todos });
-    },
-    e => {
-      res.status(400).send(e);
-    }
-  );
+// app.get("/todos", (req, res) => {
+//   Todo.find().then(
+//     todos => {
+//       res.send({ todos });
+//     },
+//     e => {
+//       res.status(400).send(e);
+//     }
+//   );
+// });
+
+// Get /todos/123445
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      return res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 let PORT = 3000;
